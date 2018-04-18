8c8553ec04da1c2dbe365a18c24b20e9726c9e24
Brunoo-Monteiro <38073389+Brunoo-Monteiro@users.noreply.github.com>
2018-04-18 20:48:43 -0300

    Delete Lista01_01.java

--- Lista01_01.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Lista01_01{
-	public static void main(String[] args){
-		Scanner ler = new Scanner(System.in);
-		float v;
-		v = ler.nextFloat();
-		System.out.println(v * 3.6);
-	}
-}
